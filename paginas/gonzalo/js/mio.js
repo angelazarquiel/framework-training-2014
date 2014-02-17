695090c50c22ad7695745c9062eb050e744b9aef
angelazarquiel <amcanadas@edu.jccm.es>
2014-02-17 13:43:58 +0100

    tercera actualizacion

---
+++ paginas/gonzalo/js/mio.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    var my_posts = $("[rel=tooltip]");
+    for(i=0;i<my_posts.length;i++){
+        the_post = $(my_posts[i]);
+        if(the_post.hasClass('invert')){
+            the_post.tooltip({ placement: 'left'});
+            the_post.css("cursor","pointer");
+        }else{
+            the_post.tooltip({ placement: 'rigt'});
+            the_post.css("cursor","pointer");
+        }
+    }
+});
+$(function() {
+    $('.panel-image img.panel-image-preview').on('click', function(e) {
+        $(this).closest('.panel-image').toggleClass('hide-panel-body');
+    });
+});
+// Stop carousel
+$('.carousel').carousel({
+  interval: false
+});
+$(document).ready(function(){
+    //FANCYBOX
+    //https://github.com/fancyapps/fancyBox
+    $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+});
+$(document).ready( function() {
+    $('#myCarousel').carousel({
+        interval:   4000
+    });
+    
+    var clickEvent = false;
+    $('#myCarousel').on('click', '.nav a', function() {
+            clickEvent = true;
+            $('.nav li').removeClass('active');
+            $(this).parent().addClass('active');        
+    }).on('slid.bs.carousel', function(e) {
+        if(!clickEvent) {
+            var count = $('.nav').children().length -1;
+            var current = $('.nav li.active');
+            current.removeClass('active').next().addClass('active');
+            var id = parseInt(current.data('slide-to'));
+            if(count == id) {
+                $('.nav li').first().addClass('active');    
+            }
+        }
+        clickEvent = false;
+    });
+});
+   
+  
